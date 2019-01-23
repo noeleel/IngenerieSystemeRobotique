ca290735b1ff3e80373c001fd4cd40adeb0031e9
phoenix1249 <maxime.do@ensta-bretagne.org>
2019-01-23 10:11:02 +0100

    Task 4.5/4.7 PID première implementation

--- desherborator_control/scripts/regulation.py
+++ desherborator_control/scripts/regulation.py
@@ -5,16 +5,20 @@ import numpy
 from std_msgs.msg import Bool
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Pose2D
+from geometry_msgs.msg import Quaternion
 
 Distance = 0
 Angle = 0
 offset = 0.2598
 Commande = Twist()
-kp_d = 1
-kd_d = -0.1
+kp_d = 0.1
+kd_d = -0.01
 
-kp_a = 1
+kp_a = 0.5
 kd_a = -0.1
+
+V_MAX = 0.25
+OMEGA_MAX = 0.5
 
 
 def callback(data):
@@ -23,17 +27,32 @@ def callback(data):
     global Angle
     Angle = data.theta
 
+def pid_callback(data):
+    global kp_d
+    kp_d = data.x
+
+    global kd_d
+    kp_d = data.x
+
+    global kp_a
+    kp_d = data.x
+
+    global kd_a
+    kp_d = data.x
+
+
 
 
 def regulation():
+    rospy.init_node("regulation",anonymous=True)
 
     pub  = rospy.Publisher("cmd_vel",Twist)
     goal = rospy.Publisher("OnZone",Bool)
-    rospy.init_node("regulation",anonymous=True)
     rate = rospy.Rate(10)
 
 
     rospy.Subscriber("DistAngle", Pose2D,callback)
+    rospy.Subscriber("PID",Quaternion,pid_callback)
 
     
     while not rospy.is_shutdown():
@@ -41,17 +60,22 @@ def regulation():
         stop = 0
 
         if Distance == 0:
-            Commande.linear.x  = 0
-            Commande.angular.z = 1
+            x  = 0
+            theta = 1
         else:
             if Distance <= offset :
-                Commande.linear.x  = 0
-                Commande.angular.z = kp_a * Angle + kd_a * Commande.angular.z
-                if Commande.angular.z <= 0.05:
+                x  = 0
+                theta = kp_a * Angle + kd_a * Commande.angular.z
+                if theta <= 0.01:
+                    theta = 0
                     stop = 1
             else:
-                Commande.linear.x  = kp_d * (Distance - offset) + kd_d * Commande.linear.x
-                Commande.angular.z = kp_a * Angle + kd_a * Commande.angular.z
+                x  = kp_d * (Distance - offset) + kd_d * Commande.linear.x
+                theta = kp_a * Angle + kd_a * Commande.angular.z
+        
+        Commande.linear.x = min(x,V_MAX)
+        Commande.angular.z = min(max(theta,-OMEGA_MAX),OMEGA_MAX)
+
 
         goal.publish(stop)
         pub.publish(Commande)
