3a7e25372b045fe189b997fe0f9d341504e469ab
houdeval <alexandre.houdeville@ensta-bretagne.org>
2019-01-31 10:40:14 +0100

    task destroy weeds

--- desherborator_gazebo/script/weed_generator.py
+++ desherborator_gazebo/script/weed_generator.py
@@ -4,8 +4,13 @@ from random import uniform
 import sys
 import rospy
 from gazebo_msgs.srv import SpawnModel, SpawnModelRequest, SpawnModelResponse
+from math import sqrt
+
 
 def generer_herbe (fichier, nombre_plantes=1):
+
+    global dictionary
+
 	rospy.init_node("weed_spawner")
 	rospy.wait_for_service("/gazebo/spawn_urdf_model")
 	spawnerService = rospy.ServiceProxy("/gazebo/spawn_urdf_model", SpawnModel)
@@ -21,6 +26,9 @@ def generer_herbe (fichier, nombre_plantes=1):
 		x_herbe = uniform(-10,10)
 		y_herbe = uniform(-10,10)
 		z_herbe = 0.1/2
+
+        dictionary["Weed_{}".format(i)] = [x_herbe, y_herbe]
+
 		request.initial_pose.position.x = x_herbe
 		request.initial_pose.position.y = y_herbe
 		request.initial_pose.position.z = z_herbe
@@ -29,8 +37,31 @@ def generer_herbe (fichier, nombre_plantes=1):
 			rospy.logwarn("Unable to spawn weed {}: {}".format(i, response.status_message))
 
 
+def compare_coord(x,y):
+	global dictionary
+	normes = []
+	elements = []
+
+	for element in dictionary:
+		coords = dictionary.get(element)
+		norme = sqrt((x-coords[0])**2 + (y-coords[1])**2)
+		normes.append(norme)
+		elements.append(element)
+
+	indice = normes.index(min(normes))
+	name_destroyed = elements[indice]
+
+	del dictionary[name_destroyed] #pour supprimer l'entree
+
+	return name_destroyed
+
+#rosservice call gazebo/delete_model '{model_name: rrbot1}' pour supprimer modele (commande ros)
+
 if __name__ == '__main__':
     #PATH = "/home/haddock/5.8/src/desherborator_gazebo/model/"
+
+    dictionary = {}
+
     PATH = sys.argv[1]
     fichier_modele = PATH+"model.urdf"
     nombre_plantes = 30
